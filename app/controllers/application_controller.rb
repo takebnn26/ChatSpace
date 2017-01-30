25e6d6d8d157c732b67e1c81c88441154e12d91d
hayato takemoto <takebnn26@gmail.com>
2017-01-30 22:47:11 +0900

    delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
   def after_sign_in_path_for(resource)
