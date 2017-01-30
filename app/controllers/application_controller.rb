dc177c84bc2c074cd481a5f736d5c2cff0c7b211
hayato takemoto <takebnn26@gmail.com>
2017-01-30 22:39:03 +0900

    devide layout into devise and application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  layout :devise_layout
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+
+
   private
+
+  def devise_layout
+    devise_controller? ? 'devise' : 'application'
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
   def after_sign_in_path_for(resource)
